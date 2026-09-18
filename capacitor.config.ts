58df0d041d6d751529e58a034574f5580b6c3127
Filu <daem0ns@pixelmon-france.fr>
2026-09-18 10:40:23 +0200

    Fix one sype ?

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'fr.pixelmon_france.daem0ns.arntags',
-  appName: 'ArnTags',
+  appName: 'Arntags',
   webDir: 'dist',
   ios: {
     contentInset: 'automatic'
